b5ac1a53b68cb9c97f5a002f3e386b396765fa23
Rui Yuan <84628470+zouma-spec@users.noreply.github.com>
2022-11-30 12:41:27 -0500

    Add files via upload

--- ADB assignment/3_Main_with_Functions.py
+++ ADB assignment/3_Main_with_Functions.py
@@ -97,9 +97,9 @@ register_table('silver_runtime_clean_table',silverPath)
 
 # Update Bronze table to Reflect the Loads
 # Step 1: Update Clean records
-Update_Clean_records()
+Update_Clean_records(silver_runtime_clean)
 # Step 2: Update Quarantined records
-Update_Quarantined_records()
+Update_Quarantined_records(silver_runtime_quarantine)
 
 # COMMAND ----------
 
